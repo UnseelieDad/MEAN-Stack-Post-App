ea5c7c5e2e08ab9694486a67ebfc210d4ab88675
Seth Martin <voiceofsethmartin@gmail.com>
2020-06-20 17:31:59 -0500

    Adding to serverjs and nodemon

--- mean-project/server.js
+++ mean-project/server.js
@@ -1,10 +1,59 @@
-// Import http functionality
-const http = require('http');
+// import the express app
 const app = require('./backend/app');
-const port = process.env.PORT || 3000;
+// import http
+const http = require('http');
+const debug = require('debug')('node-angular');
 
-app.set('port', port);
+// make sure recieved port is valid
+const normalizePort = val => {
+    var port = parseInt(val, 10);
+    
+    if (isNaN(port)) {
+        // name
+        return val;
+    }
+    
+    if (port >= 0) {
+        // port number
+        return port
+    }
+
+    return false;
+}
+
+// Handle errors more gracefully
+const onError = error => {
+    if (error.syscall !== "listen") {
+        throw error;
+    }
+    const bind = typeof addr === "string" ? "pipe " + addr : "port " + port;
+    switch(error.code) {
+        case "EACCESS":
+            console.error(bind + " reqires elevated priileges");
+            process.exit(1);
+            break;
+
+        case "EADDRINUSE":
+            console.error(bind + " is already in use");
+            process.exit(1);
+            break;
+
+        default:
+            throw error;
+    }
+};
+
+// Log that the server is listening to incomming requests
+const onListening = () => {
+    const addr = server.address();
+    const bind = typeof addr === "string" ? "pipe " + addr : "port " + port;
+    debug("Listening on " + bind);
+};
+
+const port = normalizePort(process.env.PORT || "3000");
+app.set("port", port);
+
 const server = http.createServer(app);
-
-// Listen on provider host port or 3000 if that isn't set
+server.on("error", onError);
+server.on("listening", onListening);
 server.listen(port);
