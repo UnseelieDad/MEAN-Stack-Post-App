75ca665f7a338b0d09e76b9f53adb468dff9054b
Seth Martin <voiceofsethmartin@gmail.com>
2020-06-21 17:05:06 -0500

    User saving on backend

--- mean-project/backend/routes/auth.js
+++ mean-project/backend/routes/auth.js
@@ -1,9 +1,31 @@
-const express = reuire('express');
+const express = reuire("express");
+const bcrypt = require("bcrypt");
 const router = express.Router();
+// Import the user model
+const User = require("../models/user");
 
 // Route for signup
-router.post('/signup', (req, res, next) => {
-    
+router.post("/signup", (req, res, next) => {
+  // Use bcrypt to hash the incoming password
+  bcrypt.hash(req.body.password, 10).then((hash) => {
+    const user = new User({
+      email: req.body.email,
+      password: hash, // store password hash instead of password
+    });
+    user
+      .save()
+      .then((result) => {
+        res.status(201).json({
+          message: "User created!",
+          result: result,
+        });
+      })
+      .catch((err) => {
+        res.status(500).json({
+          error: err,
+        });
+      });
+  });
 });
 
 router.post();
