86535b492b18c30d91a606fb4f9b13e062851e29
Seth Martin <voiceofsethmartin@gmail.com>
2020-06-20 22:24:34 -0500

    Set up mongodb and mongoose, created post model

---
+++ mean-project/backend/models/post.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+// Use mongoose to create a schema for the post data
+const postSchema = mongoose.Schema({
+    title: { type: String, required: true },
+    content: { type: String, required: true }
+});
+
+// Export a mongoose model using the schema
+module.exports = mongoose.model('Post', postSchema);
