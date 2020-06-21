c06a48629aa574772a3b7998d4736c5bd8854e11
Seth Martin <voiceofsethmartin@gmail.com>
2020-06-21 18:00:40 -0500

    Hooked up login to angular

--- mean-project/src/app/auth/auth.service.ts
+++ mean-project/src/app/auth/auth.service.ts
@@ -11,8 +11,16 @@ export class AuthService {
 
   // Send new user data to the backend
   createUser(email: string, password: string) {
-    const authData: AuthData = {email, password}
+    const authData: AuthData = {email, password};
     this.http.post('http://localhost:3000/api/auth/signup', authData)
+      .subscribe((response) => {
+        console.log(response);
+      });
+  }
+
+  loginUser(email: string, password: string) {
+    const authData: AuthData = {email, password};
+    this.http.post('http://localhost:3000/api/auth/login', authData)
       .subscribe((response) => {
         console.log(response);
       });
