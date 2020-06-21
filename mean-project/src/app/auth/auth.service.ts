1ffcc08e095d3c15556f28b00028efb9d56a42e8
Seth Martin <voiceofsethmartin@gmail.com>
2020-06-21 18:43:43 -0500

    Header reflects auth status

--- mean-project/src/app/auth/auth.service.ts
+++ mean-project/src/app/auth/auth.service.ts
@@ -1,19 +1,25 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { AuthData } from './auth-data.model';
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   private token: string;
+  private authStatusListener = new Subject<boolean>();
 
   constructor(private http: HttpClient) {}
 
   getToken() {
     return this.token;
   }
-  
+
+  getAuthStatusListener() {
+    return this.authStatusListener.asObservable();
+  }
+
   // Send new user data to the backend
   createUser(email: string, password: string) {
     const authData: AuthData = {email, password};
@@ -30,6 +36,7 @@ export class AuthService {
         // Get the jwt token for the logged-in user
         const token = response.token;
         this.token = token;
+        this.authStatusListener.next(true);
       });
   }
 }
