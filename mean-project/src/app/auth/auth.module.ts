4f9747e460d9f9cfff54f8c698e15733c7668d54
Seth Martin <voiceofsethmartin@gmail.com>
2020-06-21 23:47:45 -0500

    Added lazy loading for auth module

--- mean-project/src/app/auth/auth.module.ts
+++ mean-project/src/app/auth/auth.module.ts
@@ -4,12 +4,15 @@ import { SignupComponent } from './signup/signup.component';
 import { AngularMaterialModule } from '../angular-material.module';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { AuthRoutingModule } from './auth-routing.module';
 
 @NgModule({
-    declarations: [
-        LoginComponent,
-        SignupComponent,
-    ],
-    imports: [CommonModule, AngularMaterialModule, FormsModule]
+  declarations: [LoginComponent, SignupComponent],
+  imports: [
+    CommonModule,
+    AngularMaterialModule,
+    FormsModule,
+    AuthRoutingModule,
+  ],
 })
 export class AuthModule {}
