1422665ceb8f4e57793dd0ef6f0de4e6e2b1fb3d
Seth Martin <voiceofsethmartin@gmail.com>
2020-06-21 19:14:25 -0500

    Added route guards

---
+++ mean-project/src/app/auth/auth.guard.ts
@@ -0,0 +1,30 @@
+import {
+  CanActivate,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+  Router,
+} from '@angular/router';
+import { Injectable } from '@angular/core';
+import { AuthService } from './auth.service';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+    
+    constructor(private authservice: AuthService, private router: Router) {}
+    
+    // Keeps unauthenticated users from reaching pages by entering urls
+    canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ):
+    | boolean
+    | import('@angular/router').UrlTree
+    | import('rxjs').Observable<boolean | import('@angular/router').UrlTree>
+    | Promise<boolean | import('@angular/router').UrlTree> {
+        const isAuth = this.authservice.getIsAuth();
+        if (!isAuth) {
+            this.router.navigate(['/login']);
+        }
+        return isAuth;
+  }
+}
