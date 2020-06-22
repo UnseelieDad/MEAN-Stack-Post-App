f86068a86b37fcb0c1814b61c75e82e44e0e9a4f
Seth Martin <voiceofsethmartin@gmail.com>
2020-06-21 19:47:03 -0500

    Authenticating users past reload

--- mean-project/src/app/auth/auth.service.ts
+++ mean-project/src/app/auth/auth.service.ts
@@ -46,11 +46,12 @@ export class AuthService {
         if (token) {
           const expiresInDuration = response.expiresIn;
           // User's token is good for an hour
-          this.tokenTimer = setTimeout(() => {
-            this.logoutUser();
-          }, expiresInDuration * 1000);
+          this.setAuthTimer(expiresInDuration);
           this.isAuthenticated = true;
           this.authStatusListener.next(true);
+          const now = new Date();
+          const expirationDate = new Date(now.getTime() + (expiresInDuration * 1000));
+          this.saveAuthData(token, expirationDate);
           this.router.navigate(['/']);
         }
       });
@@ -62,7 +63,57 @@ export class AuthService {
     this.token = null;
     this.isAuthenticated = false;
     this.authStatusListener.next(false);
-    this.router.navigate(['/']);
     clearTimeout(this.tokenTimer);
+    this.clearAuthData();
+    this.router.navigate(['/']);
+  }
+
+  // Automatically authenticate user if info is in local storage
+  autoAuthUser() {
+    const authInformation = this.getAuthData();
+    if (!authInformation) {
+      return;
+    }
+    const now = new Date();
+    const expiresIn = authInformation.expirationDate.getTime() - now.getTime();
+    // If the token is still valid, update permissions
+    if (expiresIn > 0) {
+      this.token = authInformation.token;
+      this.isAuthenticated = true;
+      this.setAuthTimer(expiresIn / 1000);
+      this.authStatusListener.next(true);
+    }
+  }
+
+  private setAuthTimer(duration: number) {
+    console.log("Setting timer to " + duration)
+    this.tokenTimer = setTimeout(() => {
+      this.logoutUser();
+    }, duration * 1000); // expire duration in seconds needs to convert to miliseconds
+  }
+
+  // Save authentication token and expire date to persist through reloads
+  private saveAuthData(token: string, expirationDate: Date) {
+    localStorage.setItem('token', token);
+    localStorage.setItem('expiration', expirationDate.toISOString());
+  }
+
+  // clear token and expiration date from storage
+  private clearAuthData() {
+    localStorage.removeItem('token');
+    localStorage.removeItem('expiration');
+  }
+
+  // get token and exp date from local storage if it exists
+  private getAuthData() {
+    const token = localStorage.getItem('token');
+    const expirationDate = localStorage.getItem('expiration');
+    if (!token || !expirationDate) {
+      return;
+    }
+    return {
+      token,
+      expirationDate: new Date(expirationDate)
+    }
   }
 }
